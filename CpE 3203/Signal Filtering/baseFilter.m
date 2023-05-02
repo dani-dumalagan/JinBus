129dd2ec9487bba12d5fd8d6e3ca32d1affcdce9
Danica Marie Dumalagan <ddumalagan91@gmail.com>
2023-05-03 01:07:01 +0800

    [3203][Jin] Signal filtering changes

--- CpE 3203/Signal Filtering/baseFilter.m
+++ CpE 3203/Signal Filtering/baseFilter.m
@@ -1,10 +1,10 @@
 clc
 
 %% Sample input signal
-dt = 0.001;
-t = 0:dt:1;
+dt = 0.0001;
+t = 0:dt:40;
 %fclean = sin(2*pi*50*t) + sin(2*pi*120*t);
-f = audioread('Recording.m4a');
+[f, Fs] = audioread('sample4.mp3');
 
 figure('Name','Signal Filtering'); 
 subplot(4,1,1)
@@ -12,3 +12,39 @@ plot(f)
 %plot(t,f,'g','LineWidth',1.5), hold on
 %plot(t,fclean,'bl','LineWidth',0.5)
 %l1 = legend('Noisy','Clean'); set(l1,'FontSize',10)
+ylim([-1 1]); 
+xlim([0 1000000]); 
+%sound(f,Fs)
+
+%% FFT
+n = length(t);
+fhat = fft(f,n);
+PSD = fhat.*conj(fhat)/n;
+freq = 1/(dt*n)*(0:n);
+L = 1:floor(n/2);
+
+subplot(4,1,2)
+plot(freq(L),PSD(L),'c','LineWidth',1), hold on
+
+%% PSD
+%indices = PSD>0.01 & PSD<0.2;
+indices = PSD>0.1; % > more muffled, < lesser value, sharper sound
+% < the lesser the value, the lesser the bass
+% higher power intensity, bass
+PSDclean = PSD.*indices;
+fhat = indices.*fhat;
+ffilt = ifft(fhat);
+
+subplot(4,1,3); 
+plot(freq(L),PSDclean(L),'-','Color',[.5 .1 0],'LineWidth',2.5)
+
+subplot(4,1,4);
+plot(t,ffilt,'b','LineWidth',0.5)
+ylim([-1 1]); 
+
+%% Play sound
+sound(ffilt,Fs)
+
+% sampling rate = speed
+
+

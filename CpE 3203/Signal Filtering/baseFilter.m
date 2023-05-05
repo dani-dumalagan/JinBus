f7fd88af0dbafb8994fe001c4fd7e35bc3b99cb5
danid <danidumalagan17@gmail.com>
2023-05-05 19:43:13 +0800

    [3203][danid] Signal filtering activity

--- CpE 3203/Signal Filtering/baseFilter.m
+++ CpE 3203/Signal Filtering/baseFilter.m
@@ -3,110 +3,90 @@ close all;
 clear all;
 %% Sample input signal
 dt = 0.0001;
-t = 0:dt:60;
-[f, Fs] = audioread('sample4.mp3'); %128kbps
+t = 0:dt:40;
+[f, Fs] = audioread('sample5.m4a'); %128kbps
+
+
+figure('Name','Testing');
+subplot(2,2,1);
+plot(f);
+
+fh = fft(f);
+subplot(2,2,2);
+plot(abs(fh));
+
+nh = length(f);
+fhshift = (-nh/2:nh/2-1)*(Fs/nh);
+yhshift = fftshift(fh);
+subplot(2,2,3);
+plot(abs(yhshift));
+
 
 figure('Name','Signal Filtering'); 
 
-cut = f([220000:350000]);
+subplot(5,2,1);
+plot(f)
+title("Full Sample")
+xlabel("Time")
+ylabel("Amplitude")
 
-cuth = fft(cut);
-subplot(4,1,1);
-plot(cut)
-subplot(4,1,2);
-plot(abs(cuth))
+% Sample audience noise
+cut = f([220000:350000]);
+subplot(5,2,2); % display cut sample
+plot(cut) 
+title("Cut Sample")
+xlabel("Time")
+ylabel("Amplitude")
+
+cuth = fft(cut); % apply fft on cut sample
+subplot(5,2,3);
+plot(abs(cuth)) % display fft of cut sample
+title("FFT of Cut Sample")
+xlabel("Frequency")
+ylabel("Amplitude")
 
 n = length(cut);
 fshift = (-n/2:n/2-1)*(Fs/n);
 yshift = fftshift(cuth);
-subplot(4,1,3);
+subplot(5,2,4);
 plot(abs(yshift));
+title("Mirrored FFT of Cut Sample")
+xlabel("Frequency")
+ylabel("Amplitude")
+
 cuth2 = abs(cuth/n);
 cuth1 = cuth2(1:n/2+1);
 cuth1(2:end-1) = 2*cuth1(2:end-1);
 cuth3 = Fs*(0:(n/2))/n;
-%subplot(4,1,4);
 
-figure;
+subplot(5,2,5);
 plot(cuth3,cuth1);
+title("Left FFT of Cut Sample")
+xlabel("Frequency")
+ylabel("Amplitude")
 
-%tanggal
-for i = 0:n
-    cuth1(i) = 0;
-end
+% Initialize butter filter
+Fcl = 1000/(Fs/2);
+Fch = 22000/(Fs/2);
+%[y,x] = butter(5,Fcl/(Fs/2)); % lowpass
+%[z,b,a] = butter(5, Fcl/Fs, 'high'); % highpass 
+[y,x] = butter(5,[Fcl Fch],'stop');
+filtf = filter(y,x,cut);
 
-figure;
-plot(cuth3,cuth1);
+subplot(5,2,6);
+plot([y,x]);
+title("Butterworth Filter")
+xlabel("Frequency")
 
-
-
-
-figure;
-subplot(4,1,1)
-plot(f)
-ylim([-1 1]);
-
-%% FFT
-n = length(t);
-fhat = fft(f,n);
-PSD = fhat.*conj(fhat)/n;
-freq = 1/(dt*n)*(0:n);
-L = 1:floor(n/2);
-
-%%
-subplot(4,1,2)
-%plot(freq(L),PSD(L),'c','LineWidth',1), hold on
-
-%% PSD
-%indices = PSD>0.01 & PSD<0.2;
-
-%% Filter powers
-indices = PSD>1; % > more muffled, < lesser value, sharper sound
-% < the lesser the value, the lesser the bass
-% higher power intensity, bass
-PSDclean = PSD.*indices;
-
-% PSDclean = PSD;
-% 
-% y = PSDclean;
-% x = (0:length(PSD)-1)*Fs/length(PSD);
-
-fhat = indices.*fhat;
-ffilt = ifft(fhat);
- 
-% n = length(f);
-% fshift = (-n/2:n/2-1)*(Fs/n);
-% yshift = fftshift(y);
-%plot(fshift,abs(yshift),'c','LineWidth',1)
-
-plot(freq(L),PSDclean(L),'-','Color',[.5 .1 0],'LineWidth',2.5)
-
-%% Filter frequencies
-cut_off = 160000;
-%for i = (length(fshift)/2)+cut_off:length(fshift)
-%    yshift(i,:) = 0;
-
-for i = cut_off:length(x)
-    y(i,:) = 0;
-end
-
-%%
-subplot(4,1,3);
-plot(abs(y),'c','LineWidth',1)
-
-%%
-subplot(4,1,4); 
-% p
-% lot(mul)
-% %
-
-ffilt = ifft(y);
-plot(ffilt,'b','LineWidth',0.5)
-ylim([-1 1]); 
+subplot(5,2,7);
+plot(filtf);
+title("Filtered Full Sample")
+xlabel("Time")
+ylabel("Amplitude")
 
 %% Play sound
 %sound(ffilt,Fs)
-%sound(f,Fs)
+%sound(filtf,Fs)
 
 % sampling rate = speed
 
